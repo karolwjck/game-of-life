ad9a22fea9ad249c3f4f00e97512f1380d919010
karol wojcik <karolwojcik29@gmail.com>
2022-06-19 20:12:25 +0100

    Added def cell_status_update to check cell logic

--- app.py
+++ app.py
@@ -37,8 +37,39 @@ def next_board_state(initial_state):
     return next_board_state
 
 
-# def cell_status_update():
+def cell_status_update(cell_coords, board_state):
+    new_board_state = dead_state()
+    x = cell_coords[0]
+    y = cell_coords[1]
+    n_alive_neighbours = 0
 
+    # Iterate around the current cells neighbours
+    for x1 in range((x-1), (x+1)+1):
+        # Check to see if you're not off the WIDTH edge of the board
+        if x<1 < 0 or x1 >= WIDTH: continue
+
+        for y1 in range((y-1), (y+1)+1):
+            # Check to see if you're not off the HEIGHT edge of the board
+            if y1 < 0 or y1 >= HEIGHT: continue
+            # Don't count the current cell as a neighbour
+            if x1 == x and y1 == y: continue
+
+            if board_state[x][y]:
+                n_alive_neighbours += 1
+
+            if board_state[x][y] == ALIVE:
+                if n_alive_neighbours <= 1:
+                    return DEAD
+                elif n_alive_neighbours <= 3:
+                    return ALIVE
+                else:
+                    return DEAD
+                
+            else:
+                if n_alive_neighbours == 3:
+                    return ALIVE
+                else:
+                    return DEAD
 
 
 def render_board_state():
